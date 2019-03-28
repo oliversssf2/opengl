838f80bdae1814af1cb7f06f8ac0cdbff0d00491
fongsu <0701289-6@g.puiching.edu.mo>
2019-03-28 09:46:30 +0800

    xsdcv

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
